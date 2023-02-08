62afcf1aaccbdd8ab0a30d0417ccc9fe9fea1d0e
kamalnine <78726439+kamalnine@users.noreply.github.com>
2023-02-08 23:57:55 +0530

    Add files via upload
    Assignment 4

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // question 1:- Lower case
+
+        //Console.WriteLine("Enter the input String: ");
+        //String s = Console.ReadLine();
+        //if (s.Length <= 20 && Regex.IsMatch(s,@"^[a-zA-Z ]+$"))
+        //{
+        //    Console.WriteLine("The output string is " + s);
+        //}
+        //else
+        //{
+        //    Console.WriteLine("String length is greater than 20 or the string contains character other than letters");
+        //}
+
+
+        // question 2: Converting text to sms language
+
+        //Console.WriteLine("Enter a message: ");
+        //String s = Console.ReadLine();
+        //string o1 = "yes";
+        //string n1 = "s";
+        //string o2 = "you";
+        //string n2 = "u";
+        //string o3 = "today";        
+        //string n3 = "2day";
+        //string o4 = "why";
+        //string n4 = "y";
+
+        //s = s.Replace(o1, n1);
+        //s = s.Replace(o2, n2);
+        //s = s.Replace(o3, n3);
+        //s = s.Replace(o4, n4);
+        //Console.WriteLine(s);
+
+
+        //question 3: String API : split() : Illustration
+
+       //Console.WriteLine("Enter the String: ");
+       // string s = Console.ReadLine();
+       // string[] words = s.Split(" ");
+       //Console.WriteLine("The words in the string are: ");
+       // foreach (string i in words)
+       // {
+
+       //     Console.WriteLine(i);
+       // }
+
+        // question 4: String Operations-Length,Uppercase,Concat & Compare
+
+        //String s1 = Console.ReadLine();
+        //String s2 = Console.ReadLine();
+        //Console.WriteLine("The length of the first string is " + s1.Length);
+        //Console.WriteLine("The length of the second string is " + s2.Length);
+        //Console.WriteLine("The uppercase of the first string is " + s1.ToUpper());
+        //Console.WriteLine("The uppercase of the second string is " + s2.ToUpper());
+        //Console.WriteLine("The concatenated string is " + s1 + s2);
+        //if (s1.Equals(s2))
+        //{
+        //    Console.WriteLine("Both the imput strings are same");
+        //}
+        //else
+        //{
+        //    Console.WriteLine(s1 + " appears before " + s2);
+        //}
+
+
+
+
+
+    }
+}
